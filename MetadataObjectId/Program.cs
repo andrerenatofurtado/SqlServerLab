406ac1ec71eca4e5e0c377c3e3ef5c48d0fe2cb8
André Furtado <andre.furtado@saipher.com.br>
2026-01-03 21:57:40 -0300

    Commit

---
+++ MetadataObjectId/Program.cs
@@ -0,0 +1,27 @@
+﻿using SqlCore.Engine;
+
+namespace MetadataObjectId
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generate next Object ID (metadata)
+
+            GenerateNextMetadataObjectId();
+        }
+
+        static void GenerateNextMetadataObjectId()
+        {
+            Console.Write("Enter a Object ID: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Next Object ID: " + Metadata.GenerateNextMetadataObjectId((int.Parse(input))));
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ SqlCore/Engine/Metadata.cs
@@ -0,0 +1,17 @@
+﻿namespace SqlCore.Engine
+{
+    public static class Metadata
+    {
+        private static int seed = 0xF42439;
+
+        public static int GenerateNextMetadataObjectId(int objid)
+        {
+            long result = (long)objid + seed;
+
+            if (result <= int.MaxValue)
+                return (int)result;
+
+            return (int)(result - int.MaxValue - 1);
+        }
+    }
+}
